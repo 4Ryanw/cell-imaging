037af8a1336f0f2d98045629104bf8333269892c
wenran <wenran@bigdatacd.com>
2024-05-14 14:23:40 +0800

    fix: 脚本修改

--- main/CellBuild.sh
+++ main/CellBuild.sh
@@ -1,7 +1,10 @@
 #上传：
-scp -r /Users/narnew/Downloads/dist.zip root@47.109.33.12:/home/project/qianduan
+scp -r /Users/narnew/Downloads/dist.zip root@47.109.33.12:/home/project/cell-imaging-system/qianduan
 #删除 解压 启动
-ssh -i ~/.ssh/id_rsa root@47.109.33.12 "cd /home/project/qianduan/; rm -rf dist/; unzip dist.zip;node server.js;"
+
+
+
+ssh -i ~/.ssh/id_rsa root@47.109.33.12 "cd /home/project/cell-imaging-system/qianduan; lsof -t -i:3000 | xargs kill -9; rm -rf dist/; unzip dist.zip;node server.js;"
 
 
 
